2c2f0e3925cb356711c0b77b36e6121e71f79d3a
pop-ketle <popqjim@gmail.com>
2020-12-08 13:47:11 +0900

    publisherとDeveloperが同じか違うかで売り上げに違いが出るのかを可視化した

--- eda.py
+++ eda.py
@@ -1,19 +1,22 @@
+import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
+#最大表示列数の指定（ここでは50列を指定）
+pd.set_option('display.max_columns', 50)
+
 train = pd.read_csv('./features/train.csv')
+test  = pd.read_csv('./features/test.csv')
 
-names = ['LEGO Marvel Super Heroes', 'Ratatouille', 'Cars', 'The LEGO Movie Videogame', 'Lego Batman 3: Beyond Gotham']
-for name in names:
-    platforms, sales = [], []
+same_pub_dev = train[train['Publisher']==train['Developer']]
+diff_pub_dev = train[train['Publisher']!=train['Developer']]
 
-    queried = train.query(f'Name=="{name}"')
-
-    for i in range(len(queried)):
-        row = queried.iloc[i]
-#     print(queried['Platform'])
-#     print(len(queried))
-#     # print(queried)
-#     for platform in queried['Platform'].tolist():
-#         platforms.append(platform)
-# print(set(platforms))
+# 予測値の可視化
+fig, ax = plt.subplots(figsize=(8, 8))
+sns.distplot(np.log1p(same_pub_dev['Global_Sales']), label=f'same: {len(same_pub_dev)}')
+sns.distplot(np.log1p(diff_pub_dev['Global_Sales']), label=f'different: {len(diff_pub_dev)}')
+ax.legend()
+ax.grid()
+plt.savefig(f'./figs/difference_of_the_pub_and_dev.png')
+plt.show()
