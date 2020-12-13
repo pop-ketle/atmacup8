fd6229767ae7eace63599dcefafc5fbb0666ef95
pop-ketle <popqjim@gmail.com>
2020-12-13 17:34:24 +0900

    new_baseline.py cv:0.8057884338101357 public:0.8682

--- best_baseline.py
+++ best_baseline.py
@@ -6,6 +6,7 @@ import pandas as pd
 import seaborn as sns
 import lightgbm as lgbm
 import optuna
+# from optuna.integration.lightgbm import LightGBMTunerCV as lightgbm_tuner
 from optuna.integration import lightgbm as lightgbm_tuner
 import matplotlib.pyplot as plt
 import texthero as hero
@@ -20,11 +21,9 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_squared_log_error
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-
 
 import warnings
 warnings.simplefilter('ignore')
@@ -35,6 +34,7 @@ RANDOM_SEED = 72
 train = pd.read_csv('./features/train.csv')
 test  = pd.read_csv('./features/test.csv')
 
+# TODO: ここ最後に回して、カラム削除もやってしまえばいいのでは
 def target_encoding(train, test, target_col, y_col, validation_col):
     # 学習データ全体でカテゴリにおけるyの平均を計算
     data_tmp    = pd.DataFrame({'target': train[target_col], 'y': train[y_col]})
@@ -57,14 +57,37 @@ def target_encoding(train, test, target_col, y_col, validation_col):
 for target in ['Platform', 'Genre', 'Rating']:
     target_encoding(train, test, target, 'Global_Sales', 'Publisher')
 
+'''頭がバグったのでちょっと中止　そもそもリークするのか？ いや、もちろん可能性はあるけど...
+# trainにしかない特徴量 ['EU_Sales', 'Global_Sales', 'JP_Sales', 'NA_Sales', 'Other_Sales']を使う
+# リークの可能性があるので、target encodingの要領でfoldを分けて集計する
+# def make_sales_portfolio(train, test, target_col, y_col, validation_col):
+    # 学習データ全体でジャンルごとに正規化した各Salesの情報を得る
+    # data_tmp = pd.DataFrame({'Genre': train[target_col], 'y': train[y_col]})
+    # _df = data_tmp.groupby('Genre')['y'].agg(['mean', 'max', 'sum'])
+    # _df = _df.add_prefix(f'{y_col}_').add_suffix(f'_std_of_{target_col}') # カラム名意味わからないけどしゃあない
+    # _df = ((_df - _df.min()) / (_df.max() - _df.min())).reset_index() # ジャンルごとに正規化
+    # test = pd.merge(test, _df, on='Genre', how='left')
+    # # 変換後の値を格納する配列を準備
+    # tmp = np.repeat(np.nan, train.shape[0])
+    # skf = StratifiedKFold(n_splits=N_SPLITS, random_state=RANDOM_SEED, shuffle=True)
+    # for train_idx, test_idx in skf.split(train, train[validation_col]):
+    #     # 学習データに対して、各カテゴリにおける目的変数の平均を計算
+    #     # _df = data_tmp.iloc[train_idx].groupby('Genre')['y'].agg(['mean', 'max', 'sum'])
+    #     _df = data_tmp.iloc[train_idx].groupby('Genre')['y'].sum()
+    #     # バリデーションデータについて、変換後の値を一時配列に格納
+    #     tmp[test_idx] = train[target_col].iloc[test_idx].map(_df)
+    #     print(_df)
+    #     print(tmp, sum(tmp))
+    #     exit()
+# for y_col in ['EU_Sales', 'Global_Sales', 'JP_Sales', 'NA_Sales', 'Other_Sales']:
+#     make_sales_portfolio(train, test, 'Genre', y_col, 'Publisher')
+#     exit()
+'''
+
 # 処理をまとめてやるためにtrainとtestを結合
 train_length = len(train) # あとで分離するように長さを保存
 train_test   = pd.concat([train, test], ignore_index=True) # indexを再定義
 # train_test   = train_test.fillna('none')
-# train_test["Publisher"] = train_test["Publisher"].replace("Unknown", '')
-train_test['Name']   = train_test['Name'].fillna('No_Title') # NameがNaNのものがあるので'No_Title'に変換
-train_test['Genre']  = train_test['Genre'].fillna('none') # floatとstrの比較になるので置換
-train_test['Rating'] = train_test['Rating'].fillna('E') # 全年齢で埋める
 
 # NameのEmbeddingsをt-sneかけたものを特徴量として加える
 embeddings_tsne = np.load('./features/sentence_embeddings_tsne.npy')
@@ -84,6 +107,9 @@ def add_tbd(df):
     return df
 
 train_test = add_tbd(train_test)
+
+# 同じNameのが出てる->プラットフォームで売り上げが分散する可能性？ 'Name'の出現回数を数えて特徴量にする、ついでに他の特徴量もいくつかcount encoding
+train_test['Name'] = train_test['Name'].fillna('No_Title') # NameがNaNのものがあるので'No_Title'に変換
 
 # PlatformとGenreを単純に文字列として結合してCountEncodingする
 train_test['Platform_and_Genre'] = train_test['Platform'] + '_' + train_test['Genre']
@@ -109,21 +135,24 @@ def count_encoding(df, target_col):
     _df = pd.DataFrame(train_test[target_col].value_counts().reset_index()).rename(columns={'index': target_col, target_col: f'CE_{target_col}'})
     return pd.merge(df, _df, on=target_col, how='left')
 
+for target_col in ['Name','Year_of_Release','Platform','Genre','Platform_and_Genre','Platform_and_Genre_and_Binning_Year']:
+    train_test = count_encoding(train_test, target_col)
+
 def label_encoding(df, target_col):
     le = preprocessing.LabelEncoder()
     df[f'LE_{target_col}'] = le.fit_transform(df[target_col])
     return df
 
+train_test['Genre']  = train_test['Genre'].fillna('none') # floatとstrの比較になるので置換
+for target_col in ['Year_of_Release','Platform','Genre']:
+    train_test = label_encoding(train_test, target_col)
+
 def onehot_encoding(df, target_col):
     _df = pd.get_dummies(df[target_col], dummy_na=False).add_prefix(f'OH_{target_col}=')
     return pd.concat([train_test, _df], axis=1)
 
-# 同じNameのが出てる->プラットフォームで売り上げが分散する可能性？ 'Name'の出現回数を数えて特徴量にする、ついでに他の特徴量もいくつかcount encoding
-for target_col in ['Name','Year_of_Release','Platform','Genre','Rating','Platform_and_Genre','Platform_and_Genre_and_Binning_Year']:
-    if target_col!='Name':
-        train_test = onehot_encoding(train_test, target_col)
-    train_test = count_encoding(train_test, target_col)
-    train_test = label_encoding(train_test, target_col)
+for target_col in ['Year_of_Release','Platform','Genre']:
+    train_test = onehot_encoding(train_test, target_col)
 
 # プラットフォームでのジャンルごとの売り上げの平均、最大、最小、合計を計算してプラットフォームでのジャンルの特徴を捉える NOTE: カウントとか効きそう？ 各国ごとに特徴量を作るのは効くのか？
 for sales in ['EU_Sales','Global_Sales','JP_Sales','NA_Sales','Other_Sales','Global_Sales']:
@@ -136,16 +165,6 @@ for sales in ['EU_Sales','Global_Sales','JP_Sales','NA_Sales','Other_Sales','Glo
     _df = pd.DataFrame(train_test.groupby(['Genre'])[sales].agg(['mean', 'max', 'min', 'sum']).reset_index())
     _df = _df.rename(columns={'mean': f'Genre_{sales}_mean', 'max': f'Genre_{sales}_max', 'min': f'Genre_{sales}_min', 'sum': f'Genre_{sales}_sum'})
     train_test = pd.merge(train_test, _df, on='Genre', how='left')
-
-    # Rating
-    _df = pd.DataFrame(train_test.groupby(['Rating'])[sales].agg(['mean', 'max', 'min', 'sum']).reset_index())
-    _df = _df.rename(columns={'mean': f'Rating_{sales}_mean', 'max': f'Rating_{sales}_max', 'min': f'Rating_{sales}_min', 'sum': f'Rating_{sales}_sum'})
-    train_test = pd.merge(train_test, _df, on='Rating', how='left')
-
-    # Year_of_Release_fillna
-    _df = pd.DataFrame(train_test.groupby(['Year_of_Release_fillna'])[sales].agg(['mean', 'max', 'min', 'sum']).reset_index())
-    _df = _df.rename(columns={'mean': f'Year_of_Release_fillna_{sales}_mean', 'max': f'Year_of_Release_fillna_{sales}_max', 'min': f'Year_of_Release_fillna_{sales}_min', 'sum': f'Year_of_Release_fillna_{sales}_sum'})
-    train_test = pd.merge(train_test, _df, on='Year_of_Release_fillna', how='left')
 
 # 'Rating'の出現回数を数えて、よく売れそうな対象年齢について考える
 _df = pd.DataFrame(train_test.groupby(['Rating'])['Global_Sales'].agg(['mean', 'max', 'min']).reset_index())
@@ -203,11 +222,6 @@ train_test = pd.merge(train_test, _df, on='Name', how='left')
 
 train_test['total_sale_count'] = train_test['sale_count'] * train_test['resale_count']
 
-# ソフト数の変遷(同年に複数タイトル売られたら相対的に売り上げが減るかも)
-_df = pd.DataFrame(train_test.groupby(['Year_of_Release_fillna'])['Year_of_Release_fillna'].agg(['count']).reset_index())
-_df = _df.rename(columns={'count': 'published_game_count_in_year'})
-train_test = pd.merge(train_test, _df, on='Year_of_Release_fillna', how='left')
-
 # 年ごとのクチコミの多さ
 _df = pd.DataFrame(train_test.groupby(['Year_of_Release'])['User_Count'].agg(['count']).reset_index())
 _df = _df.rename(columns={'count': 'Year_of_Release_User_Count_count'})
@@ -226,9 +240,9 @@ _df = pd.DataFrame(same_pub_dev_flag, columns=['same_pub_dev_flag'])
 train_test = pd.concat([train_test, _df], axis=1)
 
 # ここのPublisherの扱い方参考 https://www.guruguru.science/competitions/13/discussions/386fb2ed-f0a6-4706-85ba-7a03fedea375/
-for target_col in ['Platform','Genre','Rating','Year_of_Release']:
+for target_col in ['Platform','Genre','Year_of_Release']:
     # 各PublisherのPlatform毎のデータ件数は以下のようにして集計しています。
-    plat_pivot = train_test.pivot_table(index='Publisher', columns=target_col, values='Name', aggfunc='count').reset_index()
+    plat_pivot = train_test.pivot_table(index='Publisher', columns=target_col,values='Name', aggfunc='count').reset_index()
     plat_pivot = plat_pivot.fillna(0) # カウントだから0がいいはず
 
     # 行列の標準化
@@ -283,7 +297,8 @@ y = np.log1p(y) # log + 1 変換
 drop_column = ['Name','Platform','Genre','Publisher','NA_Sales','EU_Sales','JP_Sales','Other_Sales','Global_Sales','Developer','Rating','Platform_and_Genre','Platform_and_Genre_and_Binning_Year']
 test = test.drop(drop_column, axis=1)
 
-# # # parameter tuning
+
+# # parameter tuning
 # # lgbm
 # x_train,x_valid,y_train,y_valid = train_test_split(train, y, random_state=RANDOM_SEED, test_size=0.3)
 # # # Publisherでfoldを割るので、trainはデータを分割した後にカラムをドロップ
@@ -300,9 +315,9 @@ test = test.drop(drop_column, axis=1)
 # print(lgbm.params)
 # print(lgbm.best_iteration)
 # print(lgbm.best_score)
-# {'objective': 'regression', 'metric': 'rmse', 'importance_type': 'gain', 'feature_pre_filter': False, 'lambda_l1': 2.428871145868299e-06, 'lambda_l2': 1.4990594924106737e-06, 'num_leaves': 27, 'feature_fraction': 1.0, 'bagging_fraction': 1.0, 'bagging_freq': 0, 'min_child_samples': 20, 'num_iterations': 10000, 'early_stopping_round': 50}
-# 389
-# defaultdict(<class 'collections.OrderedDict'>, {'valid_0': OrderedDict([('rmse', 0.8075716638494324)])})
+# {'objective': 'regression', 'metric': 'rmse', 'importance_type': 'gain', 'feature_pre_filter': False, 'lambda_l1': 1.4207841579828123e-08, 'lambda_l2': 1.9057735567448798e-08, 'num_leaves': 98, 'feature_fraction': 0.5, 'bagging_fraction': 1.0, 'bagging_freq': 0, 'min_child_samples': 20, 'num_iterations': 10000, 'early_stopping_round': 50}
+# 232
+# defaultdict(<class 'collections.OrderedDict'>, {'valid_0': OrderedDict([('rmse', 0.8120248602423586)])})
 # exit()
 
 # # cab
@@ -335,8 +350,8 @@ test = test.drop(drop_column, axis=1)
 # study = optuna.create_study()
 # study.optimize(objective, n_trials = 100)
 # print(f'cab_params:{study.best_params}, best_score:{study.best_value}')
-# # # {'depth': 8, 'learning_rate': 0.03927623848920188, 'random_strength': 17, 'bagging_temperature': 2.1960884697219814, 'od_type': 'IncToDec', 'od_wait': 41}
-# # # Best is trial 15 with value: 0.8141170049482175.
+# # {'depth': 8, 'learning_rate': 0.03927623848920188, 'random_strength': 17, 'bagging_temperature': 2.1960884697219814, 'od_type': 'IncToDec', 'od_wait': 41}
+# # Best is trial 15 with value: 0.8141170049482175.
 # exit()
 
 # # デフォルトパラメータ
@@ -356,16 +371,16 @@ test = test.drop(drop_column, axis=1)
 #     'depth': 5,
 # }
 
-# optuna パラメータ
+# optunaパラメータ(boruta前)
 lgbm_params = {
     # 'objective': 'regression',
     'objective': 'rmse',
     'importance_type': 'gain',
     'feature_pre_filter': False,
-    'lambda_l1': 2.428871145868299e-06,
-    'lambda_l2': 1.4990594924106737e-06,
-    'num_leaves': 27,
-    'feature_fraction': 1.0,
+    'lambda_l1': 1.4207841579828123e-08,
+    'lambda_l2': 1.9057735567448798e-08,
+    'num_leaves': 98,
+    'feature_fraction': 0.5,
     'bagging_fraction': 1.0,
     'bagging_freq': 0,
     'min_child_samples': 20,
@@ -387,11 +402,13 @@ cab_params = {
 
 # training data の target と同じだけのゼロ配列を用意
 # float にしないと悲しい事件が起こるのでそこだけ注意
-knn_oof_pred  = np.zeros_like(y, dtype=np.float)
-cab_oof_pred  = np.zeros_like(y, dtype=np.float)
+cab_oof_pred = np.zeros_like(y, dtype=np.float)
 lgbm_oof_pred = np.zeros_like(y, dtype=np.float)
 scores, models = [], []
 skf = StratifiedKFold(n_splits=N_SPLITS, random_state=RANDOM_SEED, shuffle=True)
+# num_bins = np.int(1 + np.log2(len(train)))
+# bins = pd.cut(train['Global_Sales'], bins=num_bins, labels=False)
+# for i, (train_idx, valid_idx) in enumerate(skf.split(train, bins.values)):
 for i, (train_idx, valid_idx) in enumerate(skf.split(train, train['Publisher'])):
     x_train, x_valid = train.iloc[train_idx], train.iloc[valid_idx]
     y_train, y_valid = y.iloc[train_idx], y.iloc[valid_idx]
@@ -443,7 +460,7 @@ score = sum(scores) / len(scores)
 print(score)
 
 # ファイルを生成する前にワンクッション置きたい
-# exit();
+# exit()
 
 pred = np.array([model.predict(test) for model in models])
 pred = np.mean(pred, axis=0)
@@ -457,7 +474,6 @@ sub_df.to_csv(f'./submission/cv:{score}_sub.csv', index=False)
 # feature importanceの可視化
 feature_importance_df = pd.DataFrame()
 for i, model in enumerate(models):
-    if i%2==0: continue
     _df = pd.DataFrame()
     _df['feature_importance'] = model.feature_importances_
     _df['column'] = train.drop(drop_column, axis=1).columns
